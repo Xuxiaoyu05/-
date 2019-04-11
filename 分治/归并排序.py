7871cc4e9c0519449bc84f79a19993d49982db09
Xuxiaoyu05 <34875319+Xuxiaoyu05@users.noreply.github.com>
2019-04-11 11:33:50 +0800

    Update 归并排序.py

--- 分治/归并排序.py
+++ 分治/归并排序.py
@@ -9,9 +9,31 @@ def MergeSort(a):
     MergeSort(left)
     MergeSort(right)
     
-    # 合并
-    i, j, k = 0
+    # 合并, 时间复杂度O(n)
+    i, j, k = 0, 0, 0
     
     while i < len(left) and j < len(right):
       if left[i] <= right[j]:
-        
+        a[k] = left[i]
+        i += 1
+      else:
+        a[k] = right[j]
+        j += 1
+      k += 1
+   
+  
+    while i < len(left):
+      a[k] = left[i]
+      i += 1
+      k += 1
+    
+    while j < len(right):
+      a[k] = right[j]
+      j += 1
+      k += 1
+    
+  return a
+  
+alist = [3,5,6,3,9,8]
+
+print(MergeSort(alist))
